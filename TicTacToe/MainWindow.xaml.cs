756ed888df8929d71aabe14550574519b8aa064e
Ivan Spoljar <spoljar.ivan@outlook.com>
2019-07-12 18:46:13 +0200

    Gameplay

--- TicTacToe/MainWindow.xaml.cs
+++ TicTacToe/MainWindow.xaml.cs
@@ -27,6 +27,29 @@ namespace TicTacToe
             return i;
         }
     }
+    public static class Array
+    {
+        public static int[] val = new int[9];
+        public static void fill()
+        {
+            for (int j = 0; j < 9; j++)
+                val[j] = 0;
+        }
+        public static void done(int y, int z)
+        {
+            val[y] = z;
+            if ((val[0] == val[1] && val[0] == val[2] && val[0] == 2) || (val[0] == val[3] && val[0] == val[6] && val[0] == 2) || (val[4] == val[0] && val[0] == val[8] && val[0] == 2) || (val[1] == val[4] && val[1] == val[7] && val[1] == 2) || (val[2] == val[5] && val[2] == val[8] && val[2] == 2) || (val[3] == val[4] && val[3] == val[5] && val[3] == 2) || (val[6] == val[4] && val[6] == val[2] && val[6] == 2) || (val[6] == val[7] && val[6] == val[8] && val[6] == 2))
+            {
+                MessageBox.Show("Pobijedio je X");
+                Application.Current.Shutdown();
+            }
+            if ((val[0] == val[1] && val[0] == val[2] && val[0] == 1) || (val[0] == val[3] && val[0] == val[6] && val[0] == 1) || (val[4] == val[0] && val[0] == val[8] && val[0] == 1) || (val[1] == val[4] && val[1] == val[7] && val[1] == 1) || (val[2] == val[5] && val[2] == val[8] && val[2] == 1) || (val[3] == val[4] && val[3] == val[5] && val[3] == 1) || (val[6] == val[4] && val[6] == val[2] && val[6] == 1) || (val[6] == val[7] && val[6] == val[8] && val[6] == 1))
+            {
+                MessageBox.Show("Pobijedio je O");
+                Application.Current.Shutdown();
+            }
+        }
+    }
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -37,12 +60,21 @@ namespace TicTacToe
         private void Button_Click_00(object sender, RoutedEventArgs e)
         {
             Button obj = (Button) sender;
-            if (obj.Content == "X" || obj.Content=="O")
+            if (obj.Content == "X" || obj.Content == "O")
             {
                 MessageBox.Show("Polje je popunjeno!");
             }
-            else if (Counters.brojac(1) % 2 == 0) obj.Content = "X";
-            else obj.Content = "O";
+            else if (Counters.brojac(1) % 2 == 0)
+            {
+                obj.Content = "X";
+                Array.done(0, 2);
+            }
+            else
+            {
+                obj.Content = "O";
+                Array.done(0, 1);
+            }
+
         }
 
         private void Button_Click_01(object sender, RoutedEventArgs e)
@@ -52,8 +84,17 @@ namespace TicTacToe
             {
                 MessageBox.Show("Polje je popunjeno!");
             }
-            else if (Counters.brojac(1) % 2 == 0) obj.Content = "X";
-            else obj.Content = "O";
+            else if (Counters.brojac(1) % 2 == 0)
+            {
+                obj.Content = "X";
+                Array.done(1, 2);
+            }
+            else
+            {
+                obj.Content = "O";
+                Array.done(1, 1);
+            }
+
         }
 
         private void Button_Click_02(object sender, RoutedEventArgs e)
@@ -63,8 +104,17 @@ namespace TicTacToe
             {
                 MessageBox.Show("Polje je popunjeno!");
             }
-            else if (Counters.brojac(1) % 2 == 0) obj.Content = "X";
-            else obj.Content = "O";
+            else if (Counters.brojac(1) % 2 == 0)
+            {
+                obj.Content = "X";
+                Array.done(2, 2);
+            }
+            else
+            {
+                obj.Content = "O";
+                Array.done(2, 1);
+            }
+
         }
 
         private void Button_Click_10(object sender, RoutedEventArgs e)
@@ -74,8 +124,17 @@ namespace TicTacToe
             {
                 MessageBox.Show("Polje je popunjeno!");
             }
-            else if (Counters.brojac(1) % 2 == 0) obj.Content = "X";
-            else obj.Content = "O";
+            else if (Counters.brojac(1) % 2 == 0)
+            {
+                obj.Content = "X";
+                Array.done(3, 2);
+            }
+            else
+            {
+                obj.Content = "O";
+                Array.done(3, 1);
+            }
+
         }
 
         private void Button_Click_11(object sender, RoutedEventArgs e)
@@ -85,8 +144,17 @@ namespace TicTacToe
             {
                 MessageBox.Show("Polje je popunjeno!");
             }
-            else if (Counters.brojac(1) % 2 == 0) obj.Content = "X";
-            else obj.Content = "O";
+            else if (Counters.brojac(1) % 2 == 0)
+            {
+                obj.Content = "X";
+                Array.done(4, 2);
+            }
+            else
+            {
+                obj.Content = "O";
+                Array.done(4, 1);
+            }
+
         }
 
         private void Button_Click_12(object sender, RoutedEventArgs e)
@@ -96,8 +164,17 @@ namespace TicTacToe
             {
                 MessageBox.Show("Polje je popunjeno!");
             }
-            else if (Counters.brojac(1) % 2 == 0) obj.Content = "X";
-            else obj.Content = "O";
+            else if (Counters.brojac(1) % 2 == 0)
+            {
+                obj.Content = "X";
+                Array.done(5, 2);
+            }
+            else
+            {
+                obj.Content = "O";
+                Array.done(5, 1);
+            }
+
         }
 
         private void Button_Click_20(object sender, RoutedEventArgs e)
@@ -107,8 +184,17 @@ namespace TicTacToe
             {
                 MessageBox.Show("Polje je popunjeno!");
             }
-            else if (Counters.brojac(1) % 2 == 0) obj.Content = "X";
-            else obj.Content = "O";
+            else if (Counters.brojac(1) % 2 == 0)
+            {
+                obj.Content = "X";
+                Array.done(6, 2);
+            }
+            else
+            {
+                obj.Content = "O";
+                Array.done(6, 1);
+            }
+
         }
 
         private void Button_Click_21(object sender, RoutedEventArgs e)
@@ -118,8 +204,17 @@ namespace TicTacToe
             {
                 MessageBox.Show("Polje je popunjeno!");
             }
-            else if (Counters.brojac(1) % 2 == 0) obj.Content = "X";
-            else obj.Content = "O";
+            else if (Counters.brojac(1) % 2 == 0)
+            {
+                obj.Content = "X";
+                Array.done(7, 2);
+            }
+            else
+            {
+                obj.Content = "O";
+                Array.done(7, 1);
+            }
+
         }
 
         private void Button_Click_22(object sender, RoutedEventArgs e)
@@ -129,8 +224,18 @@ namespace TicTacToe
             {
                 MessageBox.Show("Polje je popunjeno!");
             }
-            else if (Counters.brojac(1) % 2 == 0) obj.Content = "X";
-            else obj.Content = "O";
+            else if (Counters.brojac(1) % 2 == 0)
+            {
+                obj.Content = "X";
+                Array.done(8, 2);
+            }
+            else
+            {
+                obj.Content = "O";
+                Array.done(8, 1);
+            }
+
         }
+
     }
 }
